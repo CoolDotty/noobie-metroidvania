1f03a1649edb3c2e37b170e1e5c7c908adc17c76
Karl Phillips <k@rlphilli.ps>
2023-07-29 15:53:40 -0700

    init

---
+++ addons/FMOD/editor/fmod_guids.gd
@@ -0,0 +1,18 @@
+#
+   # fmod_guids.gd - FMOD Studio API
+
+   # Generated GUIDs for project 'fmodproject.fspro'
+#
+
+class_name FMODGuids
+
+class Events:
+	const BLANKEVENT = "{e5e960c7-0c4c-4567-b82c-bac58636fe5d}"
+
+class Busses:
+	const MASTER_BUS = "{06f48557-bb7a-4f22-a9cb-9c0b44454059}"
+	const REVERB = "{d3e58489-a719-4233-a22a-3e7b3dce8591}"
+
+class Banks:
+	const MASTER = "{291a919c-4c6b-4e77-bf8c-00610e5b96d3}"
+
